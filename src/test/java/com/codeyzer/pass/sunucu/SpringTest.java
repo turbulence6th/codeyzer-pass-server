074b50e3c219e7a01c4c85d49fc4d3cdc6dd68a5
Oğuz Tanrıkulu <turbulence6th@gmail.com>
2021-10-09 13:12:19 +0300

    test eklendi

---
+++ src/test/java/com/codeyzer/pass/sunucu/SpringTest.java
@@ -0,0 +1,16 @@
+package com.codeyzer.pass.sunucu;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+        classes = CodeyzerPassSunucuUygulama.class
+)
+@ActiveProfiles("test")
+public class SpringTest {
+
+}
