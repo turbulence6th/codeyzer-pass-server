d393b602492f8579a9e2f750d59eda6843c13de2
Oğuz Tanrıkulu <turbulence6th@gmail.com>
2021-10-22 22:23:03 +0300

    datasource regex ile parçalandı

--- src/main/java/com/codeyzer/pass/sunucu/config/VeriTabaniYapilandirma.java
+++ src/main/java/com/codeyzer/pass/sunucu/config/VeriTabaniYapilandirma.java
@@ -1,9 +1,16 @@
 package com.codeyzer.pass.sunucu.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @EntityScan("com.codeyzer.pass.sunucu.entity")
 @EnableJpaRepositories("com.codeyzer.pass.sunucu.repository")
@@ -11,4 +18,31 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Configuration
 public class VeriTabaniYapilandirma {
 
+    @Value("${DATABASE_URL:postgresql://postgres:postgres@localhost:5432/codeyzer_pass?currentSchema=codeyzer_pass}")
+    private String databaseUrl;
+
+    @Bean
+    public DataSource dataSource() {
+        Matcher matcher = Pattern.compile("(?<protocol>.*?)://(?<username>.*?):(?<password>.*?)@(?<ip>.*?):(?<port>.*?)(?<path>/.*)").matcher(databaseUrl);
+        if (matcher.find()) {
+            String protocol = matcher.group("protocol");
+            String username = matcher.group("username");
+            String password = matcher.group("password");
+            String ip = matcher.group("ip");
+            String port = matcher.group("port");
+            String path = matcher.group("path");
+
+            String jdbcDbUrl = "jdbc:" + protocol + "://" + ip + ':' + port + path;
+
+            DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
+            dataSourceBuilder.driverClassName("org.postgresql.Driver");
+            dataSourceBuilder.url(jdbcDbUrl);
+            dataSourceBuilder.username(username);
+            dataSourceBuilder.password(password);
+            return dataSourceBuilder.build();
+
+        } else {
+            throw new RuntimeException("Database url hatalıdır");
+        }
+    }
 }
